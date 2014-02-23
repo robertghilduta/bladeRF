db6d3d18a1279b110b30067aa4396fc402be7f0c
Robert Ghilduta <robert.ghilduta@nuand.com>
2014-02-23 15:02:42 -0800

    tb fixup

--- hdl/fpga/ip/nuand/simulation/fx3_model.vhd
+++ hdl/fpga/ip/nuand/simulation/fx3_model.vhd
@@ -73,7 +73,8 @@ begin
     begin
         dma0_rx_reqx <= '1' ;
         dma1_rx_reqx <= '1' ;
-        dma_rx_enable <= '1' ;
+        dma_rx_enable <= '0' ;
+        wait;
         wait until rising_edge(fx3_pclk) and system_reset = '0' ;
         for i in 1 to 10 loop
             wait until rising_edge( fx3_pclk ) ;
@@ -104,14 +105,14 @@ begin
 
     tx_sample_stream : process
         constant BLOCK_SIZE : natural := 512 ;
-        variable count : natural := 0 ;
+        variable count : natural := 3170 ;
+        variable cntr : natural := 0;
     begin
         dma2_tx_reqx <= '1' ;
         dma3_tx_reqx <= '1' ;
-        dma_tx_enable <= '0' ;
+        dma_tx_enable <= '1' ;
         fx3_gpif <= (others =>'Z') ;
         wait until system_reset = '0' ;
-        wait;
         for i in 0 to 1000 loop
             wait until rising_edge( fx3_pclk ) ;
         end loop ;
@@ -122,7 +123,16 @@ begin
             wait until rising_edge( fx3_pclk ) ;
             wait until rising_edge( fx3_pclk ) ;
             dma3_tx_reqx <= '1' ;
-            for i in 1 to BLOCK_SIZE loop
+            for i in 1 to 4 loop
+              if (i = 3 or i = 2 or i = 4) then
+                  fx3_gpif <= (others => '0');
+              elsif (i = 1) then
+                  fx3_gpif(31 downto 0) <= std_logic_vector(to_signed(cntr, 32)) ;
+                  cntr := cntr + 508*2;
+              end if;
+              wait until rising_edge( fx3_pclk );            
+            end loop;
+            for i in 1 to BLOCK_SIZE - 4 loop
                 fx3_gpif(31 downto 16) <= std_logic_vector(to_signed(count, 16)) ;
                 fx3_gpif(15 downto 0) <= std_logic_vector(to_signed(-count, 16)) ;
                 count := (count + 1) mod 2048 ;
